ac0d410b37e84ecefcd8bc5316d3e6b461a0d876
David Little <david.frank.little@gmail.com>
2017-08-30 11:02:35 -0400

    Cleaned up avg_layer

--- models/chakrabarty2017/model/avg_layer.m
+++ models/chakrabarty2017/model/avg_layer.m
@@ -2,8 +2,7 @@ function y=avg_layer(x)
 y = x;
 % normalize by maximum
 row_max = max(x,[],2);
-y = y ./ row_max;
+y = bsxfun(@rdivide,y,row_max);
 
 % keep sign of final result consistent with input
-positive_rows = row_max > 0;
-y(~positive_rows,:) = -y(~positive_rows,:);
+y(row_max <= 0,:) = -y(row_max <= 0,:);
