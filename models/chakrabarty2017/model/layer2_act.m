ed42e8d521b1055a364e75459dcece14fe405571
David Little <david.frank.little@gmail.com>
2017-08-30 12:32:11 -0400

    More refactoring of layer 2

--- models/chakrabarty2017/model/layer2_act.m
+++ models/chakrabarty2017/model/layer2_act.m
@@ -1,30 +1,17 @@
 function resp = layer2_act(x,model,locs)
-ncases = size(x,1);
-ndims = size(x,2);
-
-nt = model.nt;
-batch_indices=nt+1:size(x,1);
-nc=length(batch_indices);
-data = single(x(batch_indices,:));
-past = zeros(nc,nt*ndims,'single');
-
 if nargin < 3
-  locs = 1:(nt*ndims);
+  locs = 1:(nt*size(x,2));
 end
 
-for hh=nt:-1:1 %note reverse order
-  past(:,ndims*(nt-hh)+1:ndims*(nt-hh+1)) = x(batch_indices-hh,:);
-end
-
-resp=zeros(nc,model.numhid);
+resp=zeros(size(x,1)-1,model.numhid);
 
 % combine responses across the mixture components
 for cc=1:model.numcomp
   % why calculate these? they're not used right now....
-  % v_biases = repmat(model.visbiases(cc,locs),nc,1) + ...
+  % v_biases = repmat(model.visbiases(cc,locs),size(x,1)-1,1) + ...
   %     past*model.pastvis(locs,locs,cc);
-  h_biases = repmat(model.hidbiases(cc,:),nc,1)+ ...
-      past*model.pasthid(locs,:,cc);
+  h_biases = bsxfun(@plus,model.hidbiases(cc,:),...
+                    x(1:end-1,:)*model.pasthid(locs,:,cc));
 
-  resp=resp+(data*model.vishid(locs,:,cc) + h_biases);
+  resp=resp+(x(2:end,:)*model.vishid(locs,:,cc) + h_biases);
 end
