7fda98577c4eb23e9d40eadbf127580334c0ed24
David Little <david.frank.little@gmail.com>
2017-09-01 13:19:52 -0400

    Cleaned up continuous buildup script

--- models/little/buildup_cont.jl
+++ models/little/buildup_cont.jl
@@ -18,7 +18,7 @@ freqs=[500Hz 750Hz 1000Hz 1250Hz 1500Hz 1750Hz 2000Hz];
 duration = 10s
 durations = linspace(500.0ms,duration,20)
 
-taus = 1:3
+taus = 1:2
 
 aba_seq_offset = 4tone_len
 
@@ -65,26 +65,26 @@ info("Simulating Streaming...")
     hebb = run(model,taus,stims[i,j])
     for (k,dur) in enumerate(durations)
       # compare most recent a at the start of a triplet to the most recent b
-      for h in taus
+      for tau in taus
         N_triplets = floor((dur-aba_seq_offset)/(4tone_len))
         a_onset = aba_seq_offset + 4tone_len*(N_triplets-1)
 
         b_onset = a_onset + tone_len
         ap_onset = a_onset + 2tone_len
 
-        afrom = time_to_frame(a_onset,h)
-        apfrom = time_to_frame(ap_onset,h)
-        bfrom = time_to_frame(b_onset,h)
+        afrom = time_to_frame(a_onset,tau)
+        apfrom = time_to_frame(ap_onset,tau)
+        bfrom = time_to_frame(b_onset,tau)
 
-        len = max(1,time_to_frame(tone_len,h))
+        len = max(1,time_to_frame(tone_len,tau))
 
         a_range = afrom:(afrom+len-1)
         ap_range = apfrom:(apfrom+len-1)
         b_range = bfrom:(bfrom+len-1)
 
-        hebb_dist[i,j,k,h,:] =
-          [vecnorm(hebb[h][a_range,:]-hebb[h][b_range,:]),
-           vecnorm(hebb[h][a_range,:]-hebb[h][ap_range,:])]
+        hebb_dist[i,j,k,tau,:] =
+          [vecnorm(hebb[tau][a_range,:]-hebb[tau][b_range,:]),
+           vecnorm(hebb[tau][a_range,:]-hebb[tau][ap_range,:])]
       end
     end
   end
@@ -92,7 +92,7 @@ end
 
 Base.squeeze(f,A,dims) = squeeze(f(A,dims),dims)
 
-shebb = squeeze(sum,hebb_dist[:,:,:,1:2,:],4)
+shebb = squeeze(sum,hebb_dist[:,:,:,:,:],4)
 normed = shebb[:,:,:,1] ./ shebb[:,:,:,2]
 responses = respond(normed,0.9,0.4^2)
 
