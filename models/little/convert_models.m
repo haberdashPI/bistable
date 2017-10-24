a8da9ad4d86b1cf6bb4f8651df9770f488b6175f
David Little <david.frank.little@gmail.com>
2017-10-24 10:01:37 -0400

    Store visible biases in new model file format

--- models/little/convert_models.m
+++ models/little/convert_models.m
@@ -1,5 +1,6 @@
 % converts models to julia readable format
 base_dir = '/Volumes/Data/Little_Bistable_2017_08_15/deb';
+addpath('model')
 % aud_model = init_model(base_dir,1);
 
 result_dir = '/Users/davidlittle/Data/';
@@ -12,6 +13,9 @@ h5write(file,'/layer1/W', layer_1.W{1});
 h5create(file,'/layer1/b',size(layer_1.biases{2}'));
 h5write(file,'/layer1/b',layer_1.biases{2}');
 
+h5create(file,'/layer1/bv',size(layer_1.biases{1}'));
+h5write(file,'/layer1/bv',layer_1.biases{1}');
+
 for tau_i = 1:8
   layer_2 = aud_model.layer_2{tau_i};
 
@@ -23,4 +27,7 @@ for tau_i = 1:8
 
   h5create(file,['/layer2/tau' num2str(tau_i) '/b'],size(layer_2.hidbiases'));
   h5write(file,['/layer2/tau' num2str(tau_i) '/b'],layer_2.hidbiases');
+
+  h5create(file,['/layer2/tau' num2str(tau_i) '/bv'],size(layer_2.visbiases'));
+  h5write(file,['/layer2/tau' num2str(tau_i) '/bv'],layer_2.visbiases');
 end
