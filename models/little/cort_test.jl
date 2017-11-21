c85303bd641318ac115382ed1b6ac43f2ea59460
David Little <david.frank.little@gmail.com>
2017-11-21 16:24:08 -0500

    WIP: temporal coherence, sparse, working

--- models/little/cort_test.jl
+++ models/little/cort_test.jl
@@ -5,72 +5,28 @@ include("stim.jl")
 setup_sound(sample_rate=8kHz)
 
 spect = AuditorySpectrogram("/Users/davidlittle/Data/cochba.h5")
-cort = CorticalModel(spect,rates=2.0.^(1:5),scales=2.0.^(-2:3))
-tempc = TCAnalysis(cort=cort,sparsity=0.95)
+cort = CorticalModel(spect,rates=[2,8,32,64],scales=[0.125,1,4,16])
+tempc = TCAnalysis(cort=cort,sparsity=0.96)
 
 # x = playable(sound("../../test.wav"))[0s .. 1s,:left]
 # y = cort(spect(x));
 # plot_cort(cort,y)
 
-x = aba(60ms,4,1kHz,6)
+x = aba(60ms,60ms,4,1kHz,6)
 y = cort(spect(x))
-yc = tempc(y)
+yc = tempc(y);
 
 plot_cort(cort,y)
-plot_cort(cort,yc)
+R"""
+ggsave('../../plots/run_2017_11_21/cort_6st.pdf')
+"""
 
+plot_cort(cort,yc)
+R"""
+ggsave('../../plots/run_2017_11_21/temp_6st.pdf')
+"""
 
 x = aba(60ms,4,1kHz,12);
 y = cort(spect(x));
 yc = tempc(y);
 
-plot_cort(cort,y)
-R"""
-ggsave('../../plots/run_2017_11_16/cort_12st.pdf')
-"""
-
-plot_cort(cort,yc)
-R"""
-ggsave('../../plots/run_2017_11_16/tempc_12st.pdf')
-"""
-
-
-x = aba(60ms,4,1kHz,3);
-y = cort(spect(x));
-yc = tempc(y);
-
-plot_cort(cort,y)
-R"""
-ggsave('../../plots/run_2017_11_16/cort_3st.pdf')
-"""
-
-
-plot_cort(cort,yc)
-R"""
-ggsave('../../plots/run_2017_11_16/tempc_3st.pdf')
-"""
-
-
-x = aba(60ms,100ms,4,1kHz,3);
-y = cort(spect(x));
-plot_cort(cort,y)
-R"""
-ggsave('../../plots/run_2017_11_16/cort_wgaps_3st.pdf')
-"""
-
-
-x = aba(60ms,100ms,4,1kHz,12);
-y = cort(spect(x));
-plot_cort(cort,y)
-R"""
-ggsave('../../plots/run_2017_11_16/cort_wgaps_12st.pdf')
-"""
-
-cort = CorticalModel(spect,
-                     rates=2.0.^linspace(1,5,4),
-                     scales=2.0.^linspace(-2,3,4))
-tempc = TCAnalysis(cort=cort,sparsity=0.95,τ=100ms)
-
-x = aba(60ms,75ms,4,1kHz,3);
-y = cort(spect(x))
-yc = tempc(y)
--- models/little/tempc.jl
+++ models/little/tempc.jl
@@ -1,128 +1,6 @@
 using Parameters
+using ProgressMeter
 include("cortical.jl")
-
-struct SparseCorrelationRange
-  I::Vector{Int}
-end
-function length(x::SparseCorrelationRange)
-  n = length(x.I)
-  n + (n-1)n>>1
-end
-
-start(x::SparseCorrelationRange) = (1,1)
-
-function next(x::SparseCorrelationRange,itr::NTuple{2,Int})
-  i,j = itr
-  result = CartesianIndex(i,j)
-
-  n = length(x.I)
-  if j < n
-    result,(i,j+1)
-  else
-    result,(i+1,i+1)
-  end
-end
-
-function done(x::SparseCorrelationRange,itr::NTuple{2,Int})
-  i,j = itr
-  n = length(x.I)
-
-  i >= n && j >= n
-end
-
-mutable struct SparseCorrelation
-  vals::Vector{Int}
-  II::Vector{CartesianIndex{2}}
-  len::Int
-  N::Int
-end
-
-function SparseCorrelation(N,sparsity)
-  # make a guess about how much space we'll need
-  n = ((1-sparsity)N)^1.5
-  SparseCorrelation(Array{Int}(n),Array{CartesianIndex{2}}(n),0,N)
-end
-
-function update!(C::SparseCorrelation,n)
-  if n > length(C.vals)
-    resize!(C.vals,n)
-    resize!(C.II,n)
-  end
-  C.len = n
-end
-
-indices(C::SparseCorrelation) = filter(ii -> ii[1] > 0,C.II)
-
-function sorted_merge(X,Y,lt,eq,XT,YT)
-  result = CircularDeque{Union{XT,YT}}(length(X) + length(Y))
-  state1,state2 = start(X),start(Y)
-  item1 = zero(XT)
-  item2 = zero(YT)
-
-  sorted_merge_helper!(result,X,Y,lt,eq,state1,state2,item1,item2)
-end
-
-function sorted_merge_helper!(result,X,Y,lt,eq,state1,state2,item1,item2)
-  a1,a2 = false,false
-  d1,d2 = done(state1),done(state2)
-  while !((d1 && !a1) && (d2 && !a2))
-    if !d1 && !a1 && !d2 && !a2
-      item1,state1 = next(X,state1)
-      item2,state2 = next(X,state2)
-      a1 = a2 = true
-      d1,d2 = done(state1),done(state2)
-    elseif !d2 && !a2
-      item2,state2 = next(X,state2)
-      a2 = true
-      d2 = done(state2)
-    elseif !d1 && !a1
-      item1,state1 = next(X,state2)
-      a1 = true
-      d1 = done(state1)
-    end
-
-    if lt(item1,item2)
-      a1 = false
-      push!(result,item1)
-    elseif eq(item1,item2)
-      a1 = a2 = false
-      push!(result,item1)
-    else
-      a2 = false
-      push!(result,item2)
-    end
-  end
-
-  result
-end
-
-function update_correlation(C::SparseCorrelation,x,θ,c_t)
-  large = find(x -> abs(x) > θ,x)
-  xabs = [abs(x[i]) for i in large]
-  maxabs = maximum(xabs)
-  entries = nonzero[find(i -> maxabs*xabs[i] > C.θ,nonzero)]
-
-  newi = SparseCorrelationRange(entries)
-  oldi = filter(ii -> ii[1] > 0,1:C.len)
-
-  indices = sorted_merge(newi,oldi,(i,j) -> corless(newi,C.II[j]),
-                         (i,j) -> i == C.II[j])
-
-  update!(w,length(indices))
-
-  @threads for (k,II) in enumerate(indices)
-    i,j = II.T
-    v = c_t*(x[i] * x[j] - C[i,j])
-
-    if v > C.θ
-      w.vals[k] = v
-      w.I[k] = i
-      w.J[k] = j
-    else
-      w.I[k] = 0
-    end
-  end
-end
 
 @with_kw struct TCAnalysis
   cort::CorticalModel
@@ -133,9 +11,9 @@ end
 
 # effecient algorithm to find the quantile of a sparse matrix
 # when we know there are no negative entries
-function aquantile(x::AbstractSparseMatrix,p)
+function aquantile(x::Union{SparseMatrixCSC,SparseVector},p)
   @assert 0 <= p <= 1 "p must be in the interval [0,1]"
-  _,_,nz = findnz(x)
+  nz = copy(x.nzval)
   sort!(nz)
   pastzero = false
   inzero = false
@@ -154,105 +32,49 @@ function aquantile(x::AbstractSparseMatrix,p)
   end
 end
 
-# NOTE: there is potentially a real problem with sparsity and gradual changes in
-# C.
-function (tc::TCAnalysis)(x;drop_interval=10)
+function sparsify(x::AbstractVector,θ)
+  xa = abs.(x)
+  cut = quantile(xa,θ)
+  indices = find(x -> x > cut,xa)
+  SparseVector(length(x),indices,x[indices])
+end
+
+const num_dropped = fill(0)
+const drop_min = 4 * 2^10
+function sparsify(C::SparseMatrixCSC,θ)
+  aC = abs.(C)
+  cut = aquantile(aC,θ)
+  iis = find(0 .< aC.nzval .< cut)
+  if length(iis) > 0
+    C.nzval[iis] = 0
+    if (num_dropped[] += length(iis)) > drop_min
+      num_dropped[] = 0
+      dropzeros!(C)
+    end
+  end
+end
+
+function (tc::TCAnalysis)(x)
   y = similar(x)
   N = prod(size(x)[[1,2,4]])
   C = spzeros(eltype(x),N,N)
   c_t = Δt(tc) / tc.τ
 
-  φ = 1-(1-tc.sparsity)^2
-  drop_wait = 0
-  C = SparseCorrelation()
-  for t in 1:size(x,3)
-    update_correlation(C,x,φ,c_t)
-    @views y[:,:,t,:][:] = C*x[:,:,t,:][:]
+  Csparsity = 1-(1-tc.sparsity)^2
+  C = spzeros(N,N)
+  sparsity = 0
+  @showprogress for t in 1:size(x,3)
+    xi = sparsify(@views(x[:,:,t,:][:]),tc.sparsity)
+    # NOTE: we cannot use .+= becuase this leads to problematic memory usage
+    C = C .+ c_t.*(xi .* xi' .- C)
+    xn = C*xi
+    @views y[:,:,t,:][:] = xn
 
-    aC = abs.(C)
-    cut = aquantile(aC,φ)
-    iis = find(0 .< aC .< cut)
-    C[iis] = 0
-    if length(iis) > 0
-      if (drop_wait += 1) > drop_interval
-        drop_wait = 0
-        dropzeros!(C)
-      end
-    end
+    sparsify(C,Csparsity)
+    sparsity += length(xn.nzval)
   end
+  per = round(100(1-(sparsity / prod(size(x)))))
+  perstr = per > 99 ? ">99" : string(per)
+  println("Achieved an average sparsity of $(per)%")
   y
 end
-
-
-
-# struct CorMerge{T}
-#   merged::T
-# end
-# merge_correlation_indices(x,y) = CorMerge((x,y))
-# function corless(x::CartesianIndex{2},y::CartesianIndex{2})
-#   x[1] < y[1] ? true :
-#   x[1] > y[1] ? false :
-#   x[2] < y[2]
-# end
-
-# struct CorMergeItr{T}
-#   status::Int
-#   item::NTuple{2,CartesianIndex{2}}
-#   data::T
-# end
-
-# function start(x::CorMerge)
-#   state1,state2 = start(x.merged[1]),start(x.merged[2])
-#   CorMergeItr(-done(x.merged[1],state1) - 2done(x.merged[2],state2),
-#               (CartesianIndex(0,0),CartesianIndex(0,0)),
-#               (state1,state2))
-# end
-
-# function next(x::CorMerge,state)
-#   function helper(item1,item2,state1,state2)
-#     if corless(item1,item2)
-#       if done(x.merged[1],state1)
-#         item1,CorMergeItr(-1,(item1,item2),(state1,state2))
-#       else
-#         item1,CorMergeItr(2,(item1,item2),(state1,state2))
-#       end
-#     elseif state[1] == state[2]
-#       item1,CorMergeItr(-done(x.merged[1],state1) - 2done(x.merged[2],state2),
-#                         (item1,item2),(state1,state2))
-#     else
-#       item2,CorMergeItr(1,(item1,item2),(state1,state2))
-#     end
-#   end
-
-#   if state.status == 0
-#     item1,state1 = next(x.merged[1],state.data[1])
-#     item2,state2 = next(x.merged[2],state.data[2])
-#     helper(item1,state1,item2,state2)
-#   elseif state.status == 2
-#     item1,state1 = next(x.merged[1],state.data[1])
-#     item2,state2 = state.item[2],state.data[2]
-#     helper(item1,state1,item2,state2)
-#   elseif state.status == 1
-#     item1,state1 = state.item[1],state.data[1]
-#     item2,state2 = next(x.merged[2],state.data[2])
-#     helper(item1,state1,item2,state2)
-#   elseif state.status == -1
-#     item2,state2 = next(x.merged[2],state.data[2])
-#     item1,state1 = state.item[1],state.data[1]
-#     item2,CorMergeItr(-1 - 2done(x.merged[2],item2),
-#                       (item1,item2),(state1,state2))
-#   elseif state.status == -2
-#     item1,state1 = next(x.merged[1],state.data[1])
-#     item2,state2 = state.item[2],state.data[2]
-#     item1,CorMergeItr(-2 -done(x.merged[1],item1),
-#                       (item1,item2),(state1,state2))
-#   else
-#     error("No items available")
-#   end
-# end
-# done(x::CorMerge) = x.status == -3
-
-# this is an optimized version of the following operations
-#
-# C .+= c_t.*(x .* x' .- C)
-# C[abs.(C) .<= θ] = 0
