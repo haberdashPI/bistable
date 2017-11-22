dc53c0c56b7edc4ab5d533df1c6816e1062042d1
David Little <david.frank.little@gmail.com>
2017-11-22 10:38:57 -0500

    WIP: testing TC normalization

--- models/little/cort_test.jl
+++ models/little/cort_test.jl
@@ -4,29 +4,23 @@ include("stim.jl")
 
 setup_sound(sample_rate=8kHz)
 
-spect = AuditorySpectrogram("/Users/davidlittle/Data/cochba.h5")
+spect = AuditorySpectrogram("/Users/davidlittle/Data/cochba.h5",len=25)
 cort = CorticalModel(spect,rates=[2,8,32,64],scales=[0.125,1,4,16])
-tempc = TCAnalysis(cort=cort,sparsity=0.96,frame_len=4,prior=3.0)
+tempc = TCAnalysis(cort=cort,sparsity=0.95,frame_len=3,prior=8.0,τ=100ms)
 
 # x = playable(sound("../../test.wav"))[0s .. 1s,:left]
 # y = cort(spect(x));
 # plot_cort(cort,y)
 
-x = aba(60ms,60ms,4,1kHz,6)
-y = cort(spect(x))
-yc = tempc(y);
-
-plot_cort(cort,y)
-R"""
-ggsave('../../plots/run_2017_11_21/cort_6st.pdf')
-"""
-
-plot_cort(tempc,yc)
-R"""
-ggsave('../../plots/run_2017_11_21/temp_6st.pdf')
-"""
-
-x = aba(60ms,4,1kHz,12);
+x = aba(60ms,60ms,20,1kHz,6);
 y = cort(spect(x));
 yc = tempc(y);
+
+trange = floor(Int,1.5s / Δt(spect)):floor(Int,2.5s / Δt(spect))
+rplot(spect,spect(x)[trange,:])
+
+R"quartz()"
+
+trange = floor(Int,1.5s / Δt(tempc)):floor(Int,2.5s / Δt(tempc))
+rplot(tempc,yc[trange,:,:,:])
 
