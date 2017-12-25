9eb1074d309dfb007ef26e2f389d2e2bec9cd4bd
David Little <david.frank.little@gmail.com>
2017-12-24 22:33:57 -0500

    Created some plots for separating out components.

--- models/little/cort_test.jl
+++ models/little/cort_test.jl
@@ -3,63 +3,165 @@ include("tempc.jl")
 include("stim.jl")
 setup_sound(sample_rate=8kHz)
 
+quartz() = R"quartz()"
+
 spect = AuditorySpectrogram("/Users/davidlittle/Data/cochba.h5",
-                            len=25)
+                            len=25,min_freq = 400.0Hz,max_freq = 2kHz)
 sts = [0.1,1,6,12]
 
 cort = CorticalModel(spect,scales=2.^(-1:0.5:4),bandonly=false)
-dir = "../../plots/run_2017_12_18"
-
-p = Array{Any}(3)
+dir = "../../plots/run_2017_12_24"
+mkdir(dir)
 
 # reduce the time to compute by limiting the range of frequencies
 # employed
 f_ab(st) = @>(ab(120ms,120ms,1,10,500Hz,st),attenuate(10))
 ab_f = [f_ab(st) for st in sts]
-crs = [cort(spect(ab_f[i]),usematlab=false) for i in eachindex(ab_f)];
+crs = [cort(spect(ab_f[i]),usematlab=true) for i in eachindex(ab_f)];
 
 tempc = TCAnalysis(cort,1,1s,method=:pca)
 Cs = [tempc(crs[i]) for i in eachindex(ab_f)];
 
-signal = [fusion_signal(tempc,Cs[i],crs[i])
-          for i in eachindex(ab_f)]
-# p[1] = plot_resps(signal,sts,"delta f (st)")
-p[2] = plot_resps(signal,sts,"delta f (st)")
+p = rplot(tempc,Cs,"delta f (st)" => sts)
+R"""
+library(cowplot)
+p = $p + ylab(expression(lambda[1] / sigma^2)) +
+  scale_y_continuous(breaks=c(0.0,0.25,0.5,0.75,1.0))
+save_plot($(joinpath(dir,"delta_f.pdf")),p,base_aspect_ratio=1.5)
+"""
 
-masked = mask(tempc,Cs[4][3s],crs[4],π);
+p = rplot(tempc,Cs[3][3s])
+R"""
+library(cowplot)
+save_plot($(joinpath(dir,"component_6st.pdf")),$p,base_aspect_ratio=1.2,ncol=2)
+"""
 
-rplot(cort,masked,rates=[2],scales=[2])
-rplot(cort,crs[4],rates=[2],scales=[2])
+masked = mask(tempc,Cs[3][3s],crs[3],0);
 sp = inv(cort,masked,usematlab=true)
-rplot(spect,sp)
+p = rplot(spect,sp)
+R"""
+library(cowplot)
+save_plot($(joinpath(dir,"masked_phase_0_6st.pdf")),$p,base_aspect_ratio=1.5)
+"""
 
-masked = mask(tempc,Cs[4][3s],crs[4],0);
-
-rplot(cort,masked,rates=[2],scales=[2])
-rplot(cort,crs[4],rates=[2],scales=[2])
+masked = mask(tempc,Cs[3][3s],crs[3],π);
 sp = inv(cort,masked,usematlab=true)
-rplot(spect,sp)
+p = rplot(spect,sp)
+R"""
+library(cowplot)
+save_plot($(joinpath(dir,"masked_phase_pi_6st.pdf")),$p,base_aspect_ratio=1.5)
+"""
+
+################################################################################
+# how does weighting the rates go?
+function rate_weight(cr,center)
+  r_weights = exp.(.-(log.(abs.(rates(cort))) .- log.(abs.(center))).^2 ./ 0.5log(2))
+  cr .* reshape(r_weights,1,:,1,1)
+end
+
+cr = cort(spect(ab_f[3]),usematlab=true);
+cr_r16 = rate_weight(cr,16);
+cr_r2 = rate_weight(cr,2);
+rplot(cort,cr_r16,rates=[-16,-8,-2,2,8,16],scales=[2,4,16])
+
+C = tempc(cr);
+C_r16 = tempc(cr_r16);
+C_r2 = tempc(cr_r2);
+
+p = rplot(tempc,[C,C_r16,C_r2],"Favored Rate" => ["none","16 Hz","2 Hz"])
+R"""
+library(cowplot)
+p = $p + ylab(expression(lambda[1] / sigma^2)) +
+  scale_y_continuous(breaks=c(0.0,0.25,0.5,0.75,1.0))
+save_plot($(joinpath(dir,"rates_6st.pdf")),p,base_aspect_ratio=1.5)
+"""
+
+p = rplot(tempc,C_r16[3s])
+R"""
+library(cowplot)
+save_plot($(joinpath(dir,"component_6st_rate16.pdf")),$p,base_aspect_ratio=1.2,ncol=2)
+"""
+
+p = rplot(tempc,C_r2[3s])
+R"""
+library(cowplot)
+save_plot($(joinpath(dir,"component_6st_rate2.pdf")),$p,base_aspect_ratio=1.2,ncol=2)
+"""
+
+p = Array{Any}(2)
+masked_r16 = mask(tempc,C_r16[3s],cr,0);
+sp = inv(cort,masked_r16,usematlab=true)
+p[1] = rplot(spect,sp)
+
+masked_r2 = mask(tempc,C_r2[3s],cr,0);
+sp = inv(cort,masked_r2,usematlab=true)
+p[2] = rplot(spect,sp)
+R"""
+library(cowplot)
+p = plot_grid($(p[1]) + ggtitle("Favored Rate: 16Hz"),
+          $(p[2]) + ggtitle("Favored Rate: 2Hz"),align="h",ncol=2)
+save_plot($(joinpath(dir,"maksed_rates.pdf")),p,
+  base_aspect_ratio=1.4,ncol=2)
+"""
+
+################################################################################
+# how does the scales go?
+function scale_weight(cr,center)
+  s_weights = exp.(.-(log.(scales(cort)) .- log.(center)).^2 ./ 0.5log(2))
+  cr .* reshape(s_weights,1,1,:,1)
+end
+
+cr = cort(spect(ab_f[3]),usematlab=true);
+cs16 = scale_weight(cr,16);
+cs05 = scale_weight(cr,0.5);
+
+# C = tempc(cr);
+C_s16 = tempc(cs16);
+C_s05 = tempc(cs05);
+
+p = rplot(tempc,[C,C_r16,C_r2],
+          "Favored Scale" => ["none","16 cyc/oct","0.5 cyc/oct"])
+R"""
+library(cowplot)
+p = $p + ylab(expression(lambda[1] / sigma^2)) +
+  scale_y_continuous(breaks=c(0.0,0.25,0.5,0.75,1.0))
+save_plot($(joinpath(dir,"scales_6st.pdf")),p,base_aspect_ratio=1.5)
+"""
 
 
-## testing inversion
-cr #=,Y,HS,z1,z=# = cort(spect(ab_f[4]),usematlab=false);
-rplot(cort,cr,rates=[4],scales=[2])
-sp = inv(cort,cr,usematlab=true)
-rplot(spect,sp)
+p = rplot(tempc,C_s16[3s])
+R"""
+library(cowplot)
+save_plot($(joinpath(dir,"component_6st_scale16.pdf")),$p,base_aspect_ratio=1.2,ncol=2)
+"""
 
-rplot(cort,cr,rates=[-8,-4,-2,2,4,8],scales=[2,8,16])
-
-## testing inversion: I'm lost here, why isn't this working without matlab?
-cr2 #=, Y2, HS2, z12, z2=# = cort(spect(ab_f[4]),usematlab=true);
-rplot(cort,cr2,rates=[16],scales=[2])
-sp2 = inv(cort,cr2,usematlab=true)
-rplot(spect,sp2)
-
-rplot(cort,cr2,rates=[-8,-4,-2,2,4,8],scales=[2,8,16])
+p = rplot(tempc,C_s05[3s])
+R"""
+library(cowplot)
+save_plot($(joinpath(dir,"component_6st_scale05.pdf")),$p,base_aspect_ratio=1.2,ncol=2)
+"""
 
 
-# how does weighting the scales go?
+p = Array{Any}(2)
+masked_s16 = mask(tempc,C_s16[3s],cr,0);
+sp = inv(cort,masked_s16,usematlab=true)
+p[1] = rplot(spect,sp)
 
-center = 32
-r_weights = exp.((log.(rates(cort)) .- center).^2 ./ )
-wr_cr = cr .* 
+masked_s05 = mask(tempc,C_s05[3s],cr,0);
+sp = inv(cort,masked_s05,usematlab=true)
+p[2] = rplot(spect,sp)
+
+R"""
+library(cowplot)
+p = plot_grid($(p[1]) + ggtitle("Favored Scale: 16 cyc/oct"),
+          $(p[2]) + ggtitle("Favored Scale: 0.5 cyc/oct"),align="h",ncol=2)
+save_plot($(joinpath(dir,"maksed_scales.pdf")),p,
+  base_aspect_ratio=1.4,ncol=2)
+"""
+
+# TODO: try examining these separation components when
+# applying adaptation and MI to the first layer
+
+# TODO: try examining the effects of the weighting across
+# different delta f's, is there a clear point of ambiguity
+# in separation that happens
