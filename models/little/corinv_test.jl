e7b44a6720ca833d6cd4dea0bde9d0b1a1e23026
David Little <david.frank.little@gmail.com>
2018-01-18 10:48:06 -0500

    final tests of cortical inversion

--- models/little/corinv_test.jl
+++ models/little/corinv_test.jl
@@ -17,17 +17,18 @@ tempc = TCAnalysis(cort,1,window=2s,method=:pca,frame_len=100ms)
 x = @> ab(120ms,120ms,1,10,500Hz,6) attenuate(20)
 sp = spect(x);
 cr = cort(sp);
+C = tempc(cr);
 
-cr = cort(sp,usematlab=false)
+m = mask(tempc,C[2.15s],cr[1:80,:,:,:])
 
-sp1 = inv(cort,cr,usematlab=true)
-sp2 = inv(cort,cr,usematlab=false)
+sp1,debug1 = inv(cort,m,usematlab=true)
+sp2,debug2 = inv(cort,m,usematlab=false)
 
-debug1_ = mat"gen_cort(1,256,40,[1 1]);"
-mat"HR = conj([$debug1_; zeros(256, 1)]);"
-mat"HR = [HR(1); conj(flipud(HR(2:end)))];"
-mat"HR(256+1) = abs(HR(256+2));"
+# debug1_ = mat"gen_cort(1,256,40,[1 1]);"
+# mat"HR = conj([$debug1_; zeros(256, 1)]);"
+# mat"HR = [HR(1); conj(flipud(HR(2:end)))];"
+# mat"HR(256+1) = abs(HR(256+2));"
 
-debug1_ = mat"HR + 0;"
+# debug1_ = mat"HR + 0;"
 
-debug2_ = rate_filter(-1,256,1000 / spect.len,:low,true)
+# debug2_ = rate_filter(-1,256,1000 / spect.len,:low,true)
