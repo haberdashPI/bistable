2c6a067aed244e3154a59336b85c60d4bff4da55
David Little <david.frank.little@gmail.com>
2018-02-05 16:53:01 -0500

    Fixed missing change to move plot methods to seperate file

--- models/little/packages/AuditoryCoherence/src/AuditoryCoherence.jl
+++ models/little/packages/AuditoryCoherence/src/AuditoryCoherence.jl
@@ -7,7 +7,7 @@ import AuditoryModel: Δt, Δf, times, freqs, scales, rates
 
 export adaptmi, drift, scale_weighting, ncomponents, CoherenceModel,
     fusion_ratio, object_SNR, mask, scene_object_ratio,
-    object_SNR2, ab_match, mean_spect, rplot
+    object_SNR2, ab_match, mean_spect
     
 include("online_pca.jl")
 include("adaptmi.jl")
