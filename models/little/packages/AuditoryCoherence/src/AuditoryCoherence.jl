aaa46233d5397e4bc57452e079441774db0744f9
David Little <david.frank.little@gmail.com>
2018-02-08 13:12:49 -0500

    Exported AdatpMI

--- models/little/packages/AuditoryCoherence/src/AuditoryCoherence.jl
+++ models/little/packages/AuditoryCoherence/src/AuditoryCoherence.jl
@@ -7,8 +7,8 @@ import AuditoryModel: Δt, Δf, times, freqs, scales, rates
 
 export adaptmi, drift, scale_weighting, ncomponents, CoherenceModel,
     fusion_ratio, object_SNR, mask, scene_object_ratio,
-    object_SNR2, ab_match, mean_spect
-    
+    object_SNR2, ab_match, mean_spect, AdaptMI
+
 include("online_pca.jl")
 include("adaptmi.jl")
 include("cortmi.jl")
