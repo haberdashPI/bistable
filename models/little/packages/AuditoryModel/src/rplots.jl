a31bbbc7a6b56c2db8a45f3023afa8b272521704
David Little <david.frank.little@gmail.com>
2018-02-26 09:56:08 -0500

    moved content of plots.jl into rplots.jl

--- models/little/packages/AuditoryModel/src/rplots.jl
+++ models/little/packages/AuditoryModel/src/rplots.jl
@@ -1,9 +1,81 @@
 using RCall
 export rplot, collapsed_scale_plot
+import PerceptualColourMaps: cmap
+import Colors: RGB
 
-rplot(as::AuditorySpectrogram,data::Sound) = rplot(as,as(data))
-rplot(as::AuditorySpectrogram,data::AbstractVector) = rplot(as,as(data))
-function rplot(as::AuditorySpectrogram,data::Matrix)
+R"library(ggplot2)"
+
+function raster_plot(z::Matrix;x=indices(z,1),y=indices(z,2))
+  Y = ones(x) .* y'
+  X = x .* ones(y)'
+  df = DataFrame(x = vec(X),y=vec(Y),z = vec(z))
+  raster_plot(df)
+end
+
+function raster_plot(df::DataFrame;value=:z,kwds...)
+  if any(.!iszero.(imag.(df[value])))
+    raster_plot__complex(df,value=value;kwds...)
+  else
+    raster_plot__real(df,value=value;kwds...)
+  end
+end
+
+function raster_plot__real(df::DataFrame;value=:z,x=:x,y=:y,
+                           limits=extrema(real.(df[value])),real_suffix=:_real)
+  value_r = Symbol(string(value,real_suffix))
+  df = copy(df)
+  df[value_r] = real.(df[value])
+
+  colormap = if any(df[value_r] .< 0)
+    lim = maximum(abs.(limits))
+    limits = (-lim,lim)
+    "#".*hex.(RGB.(cmap("D1")))
+  else
+    "#".*hex.(RGB.(Colors.colormap("Reds")))
+  end
+
+R"""
+
+  ggplot($df,aes_string(x=$(string(x)),y=$(string(y)),fill=$(string(value_r)))) +
+    geom_raster() +
+    scale_fill_gradientn(colors=$colormap,limits=$(collect(limits)),name="x")
+
+"""
+end
+function raster_plot__complex(df::DataFrame;value=:z,x=:x,y=:y,
+                              phase_suffix=:_phase,abs_suffix=:_abs)
+  colormap = "#".*hex.(RGB.(cmap("C6")))
+  df = copy(df)
+  z_phase = Symbol(string(value,phase_suffix))
+  z_abs = Symbol(string(value,abs_suffix))
+  df[z_phase] = angle.(df[value])
+  df[z_abs] = abs.(df[value])
+
+R"""
+
+  ggplot($df,aes_string(x=$(string(x)),y=$(string(y)),
+                        fill=$(string(z_phase)),alpha=$(string(z_abs)))) +
+    geom_raster() +
+    scale_fill_gradientn(colors=$colormap,limits=c(-pi-0.01,pi+0.01),
+                         breaks=c(-pi,0,pi),
+                         labels=c(expression(-pi),expression(0),
+                                  expression(+pi)),
+                         name = expression(Arg(x)))+
+    scale_alpha_continuous(range=c(0,1),name="|x|")
+
+"""
+end
+
+function aplot(data) =
+  @assert haskey(data,"kind") "Missing 'kind' metadata"
+  aplot(data,data["kind"])
+end
+
+aplot(data,as::AuditorySpectrogram) = rplot(as,audiospect(data,as))
+function aplot(data::ImageMetaAxis,as::AuditorySpectrogram)
+  @assert haskey(data,"kind") "Missing 'kind' metadata"
+  @assert as == data["kind"] "Metadata must match spectrogram parameters"
+
   ixs = CartesianRange(size(data))
   at(ixs,i) = map(x -> x[i],ixs)
 
