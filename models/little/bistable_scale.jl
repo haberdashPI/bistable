5a74a2ea74fec6f5c9e16e487df8a5eb95378740
David Little <david.frank.little@gmail.com>
2018-04-24 15:22:26 -0400

    Threshold based source counting works again.

--- models/little/bistable_scale.jl
+++ models/little/bistable_scale.jl
@@ -27,108 +27,28 @@ sweights = AxisArray(squeeze(mean(abs.(cs),axisdim(cs,Axis{:freq})),3),
 
 swn = drift(sweights,τ_σ = 500ms,c_σ = 0.3);
 swna,a,m = adaptmi(swn,
-                   c_m=5,τ_m=350ms,W_m=scale_weighting2(cs,0.2),
-                   c_d=1.8,τ_d = 500ms,
-                   c_e=1.5,τ_e = 300ms,
-                   c_a=30,τ_a=3s,shape_y = x -> max(0,x),α=3.0)
+                   τ_x=300ms, c_x=3.0,
+                   τ_n = 1s, c_n = 15,
+                   c_m=30,τ_m=350ms,W_m=scale_weighting(cs,10.0),
+                   c_a=10,τ_a=3s,shape_y = x -> max(0,x))
 
 csa = similar(cs);
 csa .= sqrt.(abs.(cs) .* swna) .* exp.(angle.(cs)*im)
 rplot(csa)
 
-stream1,stream2 = threshold_scales(csa,1.5cycoct)
-# counts = source_count_by_threshold(csa,cutoff=2cycoct,buildup=1s,
-#                                    window=1s,delta=0.25s)
+counts,stream1,stream2 = source_count_by_threshold(csa,cutoff=2cycoct,buildup=1s,
+                                                   window=1s,delta=0.25s,
+                                                   return_intermediate=true)
 
-#########################################
-# find object count by peak picking
-
-# first, a little experiment on a single slice
-lplot(x) = R"""
-qplot(x=1:$(length(x)),y=$(vec(x)),geom='line')
-"""
-
-slice1 = vec(abs.(mean(Array(csa[0s .. 1s,0.5cycoct,:]),1)))
-
+N = length(stream1)
+df = DataFrame(x=repeat(1:N,outer=2),y=[stream1; stream2],
+               stream=repeat(["stream1","stream2"],inner=N))
 R"""
-qplot(x=1:$(length(slice1)),y=$slice1,geom='line') +
-  geom_vline(xintercept=$(find_peaks(slice1)))
+ggplot($df,aes(x,y,color=stream)) +
+  geom_line() + scale_color_brewer(palette='Set1')
 """
 
-slice2 = vec(abs.(mean(Array(csa[5s .. 6s,3.1cycoct,:]),1)))
-
-R"""
-qplot(x=1:$(length(slice2)),y=$slice2,geom='line') +
-  geom_vline(xintercept=$(find_peaks(slice2)))
-"""
-
-win = abs.(squeeze(mean(Array(csa[0s .. 1s,:,:]),1),1))
-guess_source_count(win)
-
-win = abs.(squeeze(mean(Array(csa[5s .. 6s,:,:]),1),1))
-guess_source_count(win)
-
-counts = source_count_by_peaks(csa)
-
-# TODO: things seem to be make sense for high cyc/oct, what about low?
-p1 = rplot(source_bumps(csa[:,2.4cycoct .. 3.1cycoct,:]));
-p2 = rplot(source_peaks(csa[:,2.4cycoct .. 3.1cycoct,:],stage=:final));
-
-R"""
-plot_grid($p1,$p2,nrow=2)
-"""
-
-p1 = rplot(source_bumps(csa[:,0.5cycoct .. 0.6cycoct,:]));
-p2 = rplot(source_peaks(csa[:,0.5cycoct .. 0.6cycoct,:],stage=:final));
-
-R"""
-plot_grid($p1,$p2,nrow=2)
-"""
-counts = source_count_by_peaks(csa)
-R"""
-qplot(x=$(ustrip.(times(counts))),y=$(Array(counts)),geom='line')
-"""
-
-lens,vals = findlengths(counts)
-
-########################################
-# find object count by scales
-
-# find a good threshold
-threshold = mean(abs.(csa))
-csat = copy(csa)
-csat[abs.(csa) .< threshold] = 0
-rplot(csat)
-
-# okay... seems like a good threshold
-two_stream_scale = 2cycoct
-
-onestream = sum(max.(0,abs.(csa[:,0cycoct .. two_stream_scale,:]) .- threshold),
-                [axisdim(csa,Axis{:scale}) axisdim(csa,Axis{:freq})])
-twostream = sum(max.(0,abs.(csa[:,two_stream_scale .. 4cycoct,:]) .- threshold),
-                [axisdim(csa,Axis{:scale}) axisdim(csa,Axis{:freq})])
-
-window(fn,win,xs::Array) = [fn(xs[clamp.(i:i+win,0,end)])
-                            for i in 1:win:length(xs)]
-
-onemean = window(mean,20,onestream)
-twomean = window(mean,20,twostream)
-N = length(onemean)
-ts = times(csa)[1:20:ntimes(csa)]
-
-onemean ./= maximum(onemean[ts .> 1s])
-twomean ./= maximum(twomean)
-
-df = DataFrame(time =  ustrip.([ts; ts]),
-               sum = [vec(onemean);vec(twomean)],
-               kind = [fill("onestream",N);fill("twostream",N)])
-R"""
-ggplot($df,aes(x=time,y=sum,color=kind)) + geom_line() +
-    scale_color_brewer(palette='Set1')
-"""
-
-findlengths(x) = diff([0; find(diff(x) .!= 0) .+ 1; length(x)])
-lengths = findlengths(onemean .> twomean) .* (ts[2] - ts[1])
+lplot(counts)
 
 crsa = cortical(csa[:,:,400Hz .. 800Hz];rates=[(-2.0.^(1:5))Hz; (2.0.^(1:5))Hz])
 C = cohere(crsa,ncomponents=3,window=200ms,method=:nmf,
--- models/little/util/threshold.jl
+++ models/little/util/threshold.jl
@@ -1,24 +1,28 @@
-function threshold_scales(cs,cutoff=2cycoct,buildup=1s)
-    cs = cs[buildup .. last(times(cs))]
-  threshold = mean(abs,cs)
+function source_count_by_threshold(cs;cutoff=2cycoct,buildup=1s,window=1s,
+                                   delta=0.25s,return_intermediate=false)
+  csa = AxisArray(abs.(cs),axes(cs)...)
+  csa = csa[buildup .. last(times(cs))]
+  threshold = mean(csa) + 2std(csa)
 
   dims = (axisdim(cs,Axis{:scale}), axisdim(cs,Axis{:freq}))
-  onestream = sum(max.(0,abs.(cs[:,0cycoct .. cutoff,:]) .-
+  onestream = sum(max.(0,csa[:,0cycoct .. cutoff,:] .- threshold),dims)
+  twostream = sum(max.(0,csa[:,cutoff + 1e-8cycoct .. last(scales(cs)),:] .-
                        threshold),dims)
-  twostream = sum(max.(0,abs.(cs[:,cutoff + 1e-8cycoct .. last(scales(cs)),:]) .-
-                       threshold),dims)
 
-  onestreamax = AxisArray(squeeze(onestream,dims),Axis{:time}(times(cs)))
-  twostreamax = AxisArray(squeeze(twostream,dims),Axis{:time}(times(cs)))
+  onestream_ax = AxisArray(squeeze(onestream,dims),Axis{:time}(times(csa)))
+  twostream_ax = AxisArray(squeeze(twostream,dims),Axis{:time}(times(csa)))
 
-  onestreamax,twostreamax
-end
+  onemean = map_window(mean,onestream_ax,window,delta)
+  twomean = map_window(mean,twostream_ax,window,delta)
 
-function source_count_by_threshold(cs;cutoff=2cycoct,buildup=1s,window=1s,
-                                   delta=0.25s)
-  onestream,twostream = threshold_scales(cs,cutoff,buildup)
-  onemean = map_window(mean,onestreamax,window,delta)
-  twomean = map_window(mean,twostreamax,window,delta)
+  onemean ./= maximum(onemean)
+  twomean ./= maximum(twomean)
 
-  AxisArray((twomean .> twomean) .+ 1,Axis{:time}(times(onemean)))
+  result = AxisArray((onemean .> twomean) .+ 1,Axis{:time}(times(onemean)))
+
+  if return_intermediate
+    result, onemean, twomean
+  else
+    result
+  end
 end
