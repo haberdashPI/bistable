14f62a5f4dee1d6bbe16d5e167215c459e53d7a5
David Little <david.frank.little@gmail.com>
2018-05-11 15:31:20 -0500

    Changed around the parameter search script.

--- models/little/run_count_lengths.jl
+++ models/little/run_count_lengths.jl
@@ -1,15 +1,16 @@
 using Feather
 include("count_lengths.jl")
-
+# NEXT STEP: find all using statements and install those packages
+# on the MARCC cluster
 parameter_index = length(ARGS) > 0 ? ARGS[1] : 1
-dir = joinpath("..","..","data","count_lengths")
+dir = length(ARGS) > 1 ? ARGS[2] : joinpath("..","..","data","count_lengths")
 isdir(dir) || mkdir(dir)
 
 params = load("params.jld2")["df"]
-# N = 25
-# M = 10
-N = 1
-M = 1
+N = 25
+M = 10
+#  N = 1
+#  M = 1
 
 methods = Dict(
   # :peaks => x -> source_count_by_peaks(x,window=1s,delta=0.25s,buildup=1s),
