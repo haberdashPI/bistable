0d0608ef90c1c8932bc42d49157935df7213ef30
David Little <dlittl20@jhu.edu@login-node03.cm.cluster>
2018-07-24 10:06:21 -0400

    Scales across different thresholds

--- models/little/count_lengths/run_all_count_lengths.sh
+++ models/little/count_lengths/run_all_count_lengths.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# S=1
-S=101 # start from where N=100 leaves off
-# N=100
-N=`cat projects/bistable/models/little/count_lengths/count_lengths_N.txt`
+S=1
+# S=101 # start from where N=100 leaves off
+N=100
+# N=`cat projects/bistable/models/little/count_lengths/count_lengths_N.txt`
 # N=2000 # start with just 10 jobs for now, and see how that goes.
 K=10
 
@@ -14,7 +14,7 @@ cd
 for i in `seq $S $K $N`; do
   echo "sbatch projects/bistable/models/little/count_lengths/run_count_lengths.sh $i \
     $((i+K-1)) -r 20 -c 100 --git_hash $GIT_HASH \
-    --params projects/bistable/models/little/count_lengths/params_2018-07-16.feather \
+    --params projects/bistable/models/little/count_lengths/params_2018-07-24.feather \
     --settings projects/bistable/models/little/count_lengths/settings_2018-07-02.toml \
     -d /scratch/groups/melhila1/dlittle/bistable_threshold_001/data/ \
     -l /scratch/groups/melhila1/dlittle/bistable_threshold_001/logs/result_${i}.log"
