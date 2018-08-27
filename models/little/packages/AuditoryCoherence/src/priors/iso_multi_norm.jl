7331b0bda29fb6380d13c4c93b0d556226d8f987
David Little <david.frank.little@gmail.com>
2018-08-27 17:34:42 -0400

    Improved dimensionality checks on isoprior

--- models/little/packages/AuditoryCoherence/src/priors/iso_multi_norm.jl
+++ models/little/packages/AuditoryCoherence/src/priors/iso_multi_norm.jl
@@ -9,9 +9,10 @@ end
 Base.std(x::IsoMultiNormStats) = x.n > 0 ? x.S ./ x.n : Inf
 Base.zero(x::IsoMultiNormStats{T},C::Coherence) where T =
   IsoMultiNormStats(zero(x.μ),zero(x.S),0.0,0.0)
+coherence_dim(C::Coherence) = prod(size(C,2,3))
 
 function isonorm(prior::Coherence,n::Number,
-                   x2_offset::Number=prod(size(prior,2,3));thresh=1e-3)
+                 x2_offset::Number=coherence_dim(C);thresh=1e-3)
   data = reshape(mean(prior,4),size(prior,1),:)
   μ = squeeze(mean(data,1),1)
   S = squeeze(sum(data.^2,1),1) .* n./size(data,1) .+ n.*thresh.^2
@@ -69,8 +70,16 @@ function logpdf(stats::IsoMultiNormStats,x::AbstractVector)
   Λ = Diagonal(σ .* ((stats.n + 1)/(stats.n * nu)))
 
   if nu < 1
-    error("Insufficient data for predictive distribution, ",
-          "try collecting more data or increasing x2_offset")
+    @show size(x)
+    @show size(stats.μ)
+    @show stats.x2_offset
+    if length(x) != length(stats.μ)
+      error("Dimension mistmatch between data ($(length(x))) and ",
+            "the model ($(length(stats.μ))).")
+    else
+      error("Insufficient data for predictive distribution, ",
+            "try collecting more data or increasing x2_offset")
+    end
   end
 
   logpdf_mvt(nu,stats.μ,Λ,x)
@@ -87,7 +96,7 @@ function isonorm(prior::Number,N::Number,dims,x2_offset::Number=prod(dims))
 end
 
 function Base.zero(prior::ConstIsoPrior,C::Coherence)
-  d = prod(size(C,2,3))
+  d = coherence_dim(C)
   IsoMultiNormStats(fill(zero(prior.S),d),fill(zero(prior.S),d),0.0,0.0)
 end
 
