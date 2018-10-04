ebdbb04d22765187a3be7a6ce74e0b99293ca4ae
David Little <david.frank.little@gmail.com>
2018-10-04 11:30:24 -0400

    Created short test

--- src/test_count_lengths.jl
+++ src/test_count_lengths.jl
@@ -4,9 +4,9 @@ include("count_lengths.jl")
 datadir = joinpath(data_dir,"count_lengths","run_2018-10-03")
 count_lengths(
   datadir=datadir,
-  first_index=1,last_index=150,
-  sim_repeat=10,
-  stim_count=100,
+  first_index=1,last_index=1,
+  sim_repeat=2,
+  stim_count=5,
   params=joinpath(datadir,"individual_levels_params.feather"),
   progressbar=false
 )
