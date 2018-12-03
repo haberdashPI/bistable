54d9e1869bc1bbd9f3ed8fb7bb1ac4081e000e49
David Little <david.frank.little@gmail.com>
2018-12-03 15:42:27 -0500

    Using revise `includet` to simplify troubleshooting

--- src/setup.jl
+++ src/setup.jl
@@ -18,8 +18,8 @@ using Formatting
 const srcdir = @__DIR__
 const plotdir = joinpath(@__DIR__,"..","plots","paper")
 const grantdir = joinpath(@__DIR__,"..","plots","grant")
-include(joinpath(srcdir,"count_lengths.jl"))
-include(joinpath(srcdir,"measures.jl"))
-include(joinpath(srcdir,"plotting.jl"))
+includet(joinpath(srcdir,"count_lengths.jl"))
+includet(joinpath(srcdir,"measures.jl"))
+includet(joinpath(srcdir,"plotting.jl"))
 
 R"options(rcalljl_options=list(width=800,height=400))"
