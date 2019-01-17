dcb1bd17b9ebb7ef92fad2ecfe01fcb65db8b252
David Little <david.frank.little@gmail.com>
2019-01-17 13:51:39 -0500

    Fixed parameter generation.

--- src/test_count_lengths.jl
+++ src/test_count_lengths.jl
@@ -1,7 +1,8 @@
 using Pkg; Pkg.activate(joinpath(@__DIR__,".."))
 include("count_lengths.jl")
 
-datadir = joinpath(homedir(),"work","dlittle","bistable_individual_sensitive","run_2018-12-06")
+datadir = joinpath(homedir(),"work","dlittle",
+                   "bistable_individual_sensitive_W","run_2019-01-02")
 # datadir = joinpath(@__DIR__,"..","data","count_lengths",
 #                    "bistable_individual_levels","run_2018-11-15")
 
