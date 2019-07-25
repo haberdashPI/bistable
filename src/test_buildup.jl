e5f4d7d47428cd9669503536c978c4bdcc4e8e09
David Little <david.frank.little@gmail.com>
2019-07-25 11:06:53 -0400

    WIP: full run of buildup conditions (before fix for first stimulus)

--- src/test_buildup.jl
+++ src/test_buildup.jl
@@ -17,15 +17,39 @@ if endswith(gethostname(),".cluster")
     @everywhere include(joinpath(@__DIR__,"..","src","setup.jl"))
 end
 
-p = copy(params[select_params(params,t_c_a=5,t_c_m=5,Δf=6),:])
-p.Δf .= 6
-results = @distributed (vcat) for i in 1:4000
-    with_logger(NullLogger()) do
-        len,val = bistable_model(p,settings,intermediate_results=true).percepts.counts
+params.f_c_σ .= 0.5
+params.s_c_σ .= 0.5
+params.t_c_σ .= 0.5
+
+models = Dict(
+  :object => copy(params[select_params(params,t_c_a=5,t_c_m=5,Δf=6),:]),
+  :central => copy(params[select_params(params,s_c_a=5,s_c_m=5,Δf=6),:]),
+  :peripheral => copy(params[select_params(params,f_c_a=15,f_c_m=130,Δf=6),:]),
+)
+p = copy(models[:peripheral])
+p.f_c_σ .= 0.5
+p.s_c_a .= 5
+p.s_c_m .= 5
+p.s_c_σ .= 0.5
+p.t_c_a .= 5
+p.t_c_m .= 5
+p.t_c_σ .= 0.5
+models[:combined] = p
+
+writedir = joinpath(@__DIR__,"..","data","buildup")
+for (name,model) in models
+  for Δ in [3,6,12]
+    p = copy(model)
+    p.Δf .= Δ
+    results = @distributed (vcat) for i in 1:10^4
+      with_logger(NullLogger()) do
+        results = bistable_model(p,settings,intermediate_results=true)
+        len,val = results.percepts.counts
         DataFrame(length=len,response=val.+1,run=i)
+      end
     end
+    CSV.write(joinpath(writedir,"$(name)_df$(Δ)_$(Date(now())).csv"),results)
+  end
 end
 
-Δ = 0.05
-sdf = buildup_mean(buildup,delta=Δ,length=12)
 
