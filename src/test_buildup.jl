cae766d437b49536166a59f37589222142014244
David Little <david.frank.little@gmail.com>
2019-07-29 14:57:59 -0400

    Fix object buildup screw-up

--- src/test_buildup.jl
+++ src/test_buildup.jl
@@ -6,7 +6,7 @@ datadir = joinpath(@__DIR__,"..","data","count_lengths","run_2018-11-26")
 
 params = load_params(joinpath(datadir,"params.jld2"))
 params[!,:pindex] .= 1:size(params,1)
-settings = joinpath("..","src","settings.toml")
+settings = joinpath(@__DIR__,"..","src","settings.toml")
 settings = TOML.parsefile(settings)
 settings["stimulus"]["repeats"] = 24
 
@@ -17,24 +17,26 @@ if endswith(gethostname(),".cluster")
     @everywhere include(joinpath(@__DIR__,"..","src","setup.jl"))
 end
 
+# todo: this might be why we didn't get the right thing
+# in our object-level simulation
 params.f_c_σ .= 0.5
 params.s_c_σ .= 0.5
 params.t_c_σ .= 0.5
 
 models = Dict(
   :object => copy(params[select_params(params,t_c_a=5,t_c_m=5,Δf=6),:]),
-  :central => copy(params[select_params(params,s_c_a=5,s_c_m=5,Δf=6),:]),
-  :peripheral => copy(params[select_params(params,f_c_a=15,f_c_m=130,Δf=6),:]),
+  # :central => copy(params[select_params(params,s_c_a=5,s_c_m=5,Δf=6),:]),
+  # :peripheral => copy(params[select_params(params,f_c_a=15,f_c_m=130,Δf=6),:]),
 )
-p = copy(models[:peripheral])
-p.f_c_σ .= 0.5
-p.s_c_a .= 5
-p.s_c_m .= 5
-p.s_c_σ .= 0.5
-p.t_c_a .= 5
-p.t_c_m .= 5
-p.t_c_σ .= 0.5
-models[:combined] = p
+# p = copy(models[:peripheral])
+# p.f_c_σ .= 0.5
+# p.s_c_a .= 5
+# p.s_c_m .= 5
+# p.s_c_σ .= 0.5
+# p.t_c_a .= 5
+# p.t_c_m .= 5
+# p.t_c_σ .= 0.5
+# models[:combined] = p
 
 writedir = joinpath(@__DIR__,"..","data","buildup")
 for (name,model) in models
